5cfea4aec3296c8231333bbed864e65914659127
nathan <nathankkh@gmail.com>
2024-05-03 10:39:36 +0800

    chore: update dependencies for firebase functions

--- functions/index.js
+++ functions/index.js
@@ -15,6 +15,11 @@ const { initializeApp } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
+/* Begin V2 imports */
+const { onSchedule } = require('firebase-functions/v2/scheduler');
+const { onRequest } = require('firebase-functions/v2/https');
+/* End V2 imports */
+
 initializeApp();
 const db = getFirestore();
 const client = new SecretManagerServiceClient();
@@ -80,6 +85,7 @@ async function getData(pageNumber = 1) {
     );
     const data = await response.json();
     logger.info('data retrieved');
+    logger.debug('number of records on page ' + pageNumber + ': ' + data.pagination.page_size);
     return data;
   } catch (error) {
     logger.error(error);
@@ -108,7 +114,7 @@ function populateAttendeeTemplate(attendee) {
     4: Capitaland tenant
     5: T&C
   */
-  template.experience = attendee.answers[0].answer; // can match question_id too
+  template.experience = attendee.answers[0].answer;
   template.fieldOfInterest = attendee.answers[1].answer;
   return template;
 }
@@ -119,46 +125,53 @@ function populateAttendeeTemplate(attendee) {
  * Calls firestore to see if there is a lastUpdated doc. If so, filters out attendees who were added to the collection after the last update time.
  * Uploads all new attendees to firestore.
  * Note that this relies on Eventbrite's api to return two parent keys: pagination and attendees.
- * @param {string} collectionPath Slash separated string of the collection path. e.g. '/events/123/eventbrite'
+ * @param {string} collectionPath Slash separated string of the collection path where eventbrite attendees are stored. e.g. '/events/123/eventbrite'
  * @return {Promise<Array<Array, Number>>} A promise of an Array. The first element is an array of new attendees. The second element is the time of the most recently created attendee.
  */
 async function getNewAttendees(collectionPath) {
-  // retrieve last updated time from firestore
-  // if no last updated time, retrieve all attendees
   // if last updated time, retrieve attendees after last updated time
   // upload attendees to firestore
   // update last updated time, with the timestamp of the most recent registration
   // return new attendees
   try {
+    // get last updated time from FireStore. If it doesn't exist, set it to 0
     const lastUpdateDoc = await db.collection(collectionPath).doc('0_lastUpdated').get();
     const lastUpdateTime = lastUpdateDoc.exists ? lastUpdateDoc.data().timestamp : 0;
+
     const result = [];
+
+    // Make initial call to Eventbrite to get total number of pages.
     let currentPage = 1;
     let lastCreated = Number.NEGATIVE_INFINITY;
     // TODO: make use of initial call.
     const initialData = await getData(currentPage);
     const totalPages = initialData.pagination.page_count;
     let couldHaveCancellations = true;
+
     // iterate through pages from the back. Note that this prevents the use of continuation tokens
     for (currentPage = totalPages; currentPage >= 1; currentPage--) {
-      logger.info('page: ' + currentPage);
+      logger.debug('[Cloud fn] getNewAttendees page: ' + currentPage);
       const data = await getData(currentPage);
-      // CHECK FOR CANCELLATIONS =============================
+
+      // CHECK FOR CANCELLATIONS. Affects behaviour for exiting EB pulls early. ===================
       if (couldHaveCancellations) {
         const cancelledAttendees = data.attendees.filter((attendee) => {
           return attendee.cancelled == true;
         });
         // prevent additional checks for cancelled attendees IF the entire page has no cancellations
-        // this is valid because the API pushes cancelled attendees to the back
+        // this works because the API pushes cancelled attendees to the back of the returned data
         if (cancelledAttendees.length == 0) {
           couldHaveCancellations = false;
         }
       }
-      // =====================================================
+      // ========================================================================================
 
+      // only include attendees who were added to the collection after the last update time
       const filteredAttendees = data.attendees.filter((attendee) => {
         return new Date(attendee.created).getTime() > lastUpdateTime && attendee.cancelled == false;
       });
+
+      // populate the template and push to result array
       for (const attendee of filteredAttendees) {
         if (new Date(attendee.created).getTime() > lastCreated) {
           lastCreated = new Date(attendee.created).getTime();
@@ -169,13 +182,13 @@ async function getNewAttendees(collectionPath) {
 
       // prevent extra API calls if the most recent call had more new attendees AND there are no cancelled attendees
       if (filteredAttendees.length == 0 && couldHaveCancellations == false) {
+        logger.debug('[Cloud fn] getNewAttendees: no new attendees found for page ' + currentPage);
         break;
       }
     }
     return [result, lastCreated];
   } catch (error) {
-    logger.error(error);
-    logger.error('Error in getNewAttendees()');
+    logger.error('Error in getNewAttendees(): ' + error);
   }
 }
 
@@ -184,7 +197,9 @@ async function getNewAttendees(collectionPath) {
  * @param {String} collectionPath Slash separated string of the collection path. e.g. '/events/123/eventbrite'
  */
 async function updatePullTime(collectionPath) {
+  // Create reference to lastUpdated document.
   const lastUpdateRef = db.collection(collectionPath).doc('0_lastUpdated');
+
   try {
     await lastUpdateRef.set(
       {
@@ -192,10 +207,9 @@ async function updatePullTime(collectionPath) {
       },
       { merge: true }
     );
-    logger.info('[Cloud fn] Updated lastpull time');
+    logger.debug('[Cloud fn] Updated lastpull time');
   } catch (error) {
-    logger.error(error);
-    logger.error('Error in updatePullTime');
+    logger.error('Error in updatePullTime: ' + error);
   }
 }
 
@@ -207,12 +221,12 @@ async function updatePullTime(collectionPath) {
  * @param {number} batchSize The number of operations per batch. Default is 500.
  */
 async function uploadBatch(collectionPath, dataArray, lastUpdateTime, batchSize = 500) {
-  // TODO: UPDATE to match new folder structure
   batchSize = batchSize - 1; // -1 to account for lastUpdated doc
   const lastUpdateRef = db.collection(collectionPath).doc('0_lastUpdated');
-  logger.info('[Cloud fn] Number of records to upload: ' + dataArray.length);
+  logger.debug('[Cloud fn] Number of records to upload: ' + dataArray.length);
   const numBatches = Math.ceil(dataArray.length / batchSize);
 
+  // Split the array into batches and upload
   for (let i = 0; i < numBatches; i++) {
     try {
       const batch = db.batch(); // init batch
@@ -220,10 +234,13 @@ async function uploadBatch(collectionPath, dataArray, lastUpdateTime, batchSize 
       const endIndex = startIndex + batchSize;
       const batchArray = dataArray.slice(startIndex, endIndex); // slices the array into batches
 
+      // iterate through each attendee in batchArray and add to batch object.
       batchArray.forEach((obj) => {
         // set the name of the doc to be the id of the attendee
         const docRef = db.collection(collectionPath).doc(obj.id);
+        // add doc to the batch
         batch.set(docRef, { ...obj });
+        ``;
       });
 
       batch.set(lastUpdateRef, {
@@ -232,10 +249,11 @@ async function uploadBatch(collectionPath, dataArray, lastUpdateTime, batchSize 
         lastpull: new Date().toString()
       });
       batch.commit();
-      logger.info('batched');
+      logger.debug('batched');
     } catch (err) {
       logger.error(err);
       logger.error("Couldn't batch");
+      // No need to throw error here, as the next batch will still be attempted.
     }
   }
 }
@@ -313,3 +331,52 @@ exports.pullNewAttendeesScheduled = functions
       });
     }
   });
+
+/**
+ * Pulls new attendees from eventbrite every 5 minutes.
+ * Uses functions v2.
+ * Follows runtime options that are set in Google Cloud.
+ */
+exports.pullNewAttendeesV2 = onSchedule(
+  {
+    preserveExternalChanges: true,
+    schedule: 'every minute',
+    timeZone: 'Asia/Singapore',
+    region: 'asia-east1'
+  },
+  async () => {
+    const eventID = await getEventID();
+    const collectionName = 'eventbrite';
+    const ebCollectionPath = '/events/' + `${eventID}` + '/' + `${collectionName}`;
+    const data = await getNewAttendees(ebCollectionPath);
+    if (data) {
+      uploadBatch(ebCollectionPath, data[0], data[1]).then(() => {
+        logger.info('[CF V2] done');
+      });
+    } else {
+      updatePullTime(ebCollectionPath).then(() => {
+        logger.info('no new attendees');
+      });
+    }
+  }
+);
+
+/**
+ * Pulls new attendees from eventbrite when triggered.
+ * Manual trigger for testing purposes.
+ */
+exports.pullNewAttendeesManual = onRequest(async (request, response) => {
+  const eventID = await getEventID();
+  const collectionName = 'eventbrite';
+  const ebCollectionPath = '/events/' + `${eventID}` + '/' + `${collectionName}`;
+  const data = await getNewAttendees(ebCollectionPath);
+  if (data) {
+    uploadBatch(ebCollectionPath, data[0], data[1]).then(() => {
+      response.send('done');
+    });
+  } else {
+    updatePullTime(ebCollectionPath).then(() => {
+      response.send('no new');
+    });
+  }
+});
