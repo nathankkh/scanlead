489fb16fd5031d77e7a90eff78be0c747ce0b6c5
nathan <nathankkh@gmail.com>
2024-05-15 20:00:25 +0800

    chore: add config and sample test for jest

---
+++ functions/src/test/index.test.mjs
@@ -0,0 +1,47 @@
+
+import * as func from '../index.js';
+import * as utils from '../utils.js';
+
+process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080';
+
+/*
+const app = admin.initializeApp({
+  projectId: 'scanlead-17f3a',
+  credential: admin.credential.applicationDefault()
+});
+
+const db = app.firestore();
+db.settings({
+  host: 'localhost',
+  port: 8080
+});
+*/
+
+// run emulator separately. Running jest should trigger the test functions here
+
+describe('populateAttendeeTemplate', () => {
+  test("should return a template object with the attendee's data", () => {
+    const attendee = {
+      created: '2021-02-17T20:00:00Z',
+      order_id: '123',
+      id: '456',
+      profile: {
+        name: 'John Doe',
+        email: '',
+      }
+    };
+    const expected = {
+      created: 1613592000000,
+      id: '123456001',
+      name: 'John Doe',
+      email: '',
+      phone: '',
+      jobTitle: '',
+      experience: '',
+      fieldOfInterest: '',
+      comments: '',
+      leadType: ''
+    };
+    expect(utils.populateAttendeeTemplate(attendee)).toEqual(expected);
+  }, 10000);
+});
--- functions/src/utils.js
+++ functions/src/utils.js
@@ -17,16 +17,16 @@ const leadFields = {
 /**
  * Takes in an attendee object from an Eventbrite API pull and populates the template `leadfields`.
  * @param {*} attendee An attendee's data from an Eventbrite API pull.
- * @return {json} JSON of attendee data.
+ * @return {Object} A template object with the attendee's data.
  */
 function populateAttendeeTemplate(attendee) {
   const template = { ...leadFields };
   template.created = new Date(attendee.created).getTime();
   template.id = attendee.order_id + attendee.id + '001';
-  template.name = attendee.profile.name;
-  template.email = attendee.profile.email;
-  template.phone = attendee.profile.cell_phone;
-  template.jobTitle = attendee.profile.job_title;
+  template.name = attendee.profile.name || '';
+  template.email = attendee.profile.email || '';
+  template.phone = attendee.profile.cell_phone || '';
+  template.jobTitle = attendee.profile.job_title || '';
   /*
     0:  Work experience (years)
     1: Field of interest
@@ -35,8 +35,10 @@ function populateAttendeeTemplate(attendee) {
     4: Capitaland tenant
     5: T&C
   */
-  template.experience = attendee.answers[0].answer;
-  template.fieldOfInterest = attendee.answers[1].answer;
+  if (attendee.answers) {
+    template.experience = attendee.answers[0].answer || '';
+    template.fieldOfInterest = attendee.answers[1].answer || '';
+  }
   return template;
 }
 
